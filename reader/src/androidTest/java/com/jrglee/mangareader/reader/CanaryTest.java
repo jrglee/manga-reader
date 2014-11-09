4b5d00a81c2238c48cdba9447baf34cf08da6d08
Jorge Lee <jrg.lee@gmail.com>
2014-11-08 19:32:41 -0800

    Initial commit

---
+++ reader/src/androidTest/java/com/jrglee/mangareader/reader/CanaryTest.java
@@ -0,0 +1,13 @@
+package com.jrglee.mangareader.reader;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CanaryTest {
+
+    @Test
+    public void shouldWork() throws Exception {
+        assertThat(true).isEqualTo(true);
+    }
+}
